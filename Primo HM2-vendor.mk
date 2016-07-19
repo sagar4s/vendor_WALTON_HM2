366ddb3ba85de76d6014f16aed6f62ad0e8ee23b
sagar4s <mdshahin153@gmail.com>
2016-07-19 12:14:44 +0600

    Add files via upload

---
+++ Primo HM2-vendor.mk
@@ -0,0 +1,12 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/WALTON/Primo HM2/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/WALTON/Primo HM2/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/WALTON/Primo HM2/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/WALTON/Primo HM2/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/WALTON/Primo HM2/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/WALTON/Primo HM2/app,system/app)
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# vendor_WALTON_HM2
